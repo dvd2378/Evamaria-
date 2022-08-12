ebbb3e3b058d120602df7fbbfddd306af07d4dfe
Ichu23 <107046961+Ichu23@users.noreply.github.com>
2022-08-12 14:20:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -245,7 +245,7 @@ async def start(client, message):
     if f_caption is None:
         f_caption = f"{files.file_name}"
     buttons = [[
-            InlineKeyboardButton('𖣘 𝗠𝗢𝗩𝗜𝗘𝗦 𝗚𝗥𝗢𝗨𝗣 𖣘', url='https://t.me/new_movies_group_2021')
+            InlineKeyboardButton('☞𝐋𝐀𝐓𝐄𝐒𝐓 𝐌𝐎𝐕𝐈𝐄𝐒☜', url='https://t.me/CMT_link')
         ]]
     await client.send_cached_media(
         chat_id=message.from_user.id,
