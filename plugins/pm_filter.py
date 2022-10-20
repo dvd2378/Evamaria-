c3e912b4a472af07993de54f4b7aca7988448ccd
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2022-10-20 17:11:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -105,7 +105,7 @@ async def next_page(bot, query):
                                   callback_data="pages")]
         )
         btn.append(
-            [InlineKeyboardButton("🔮 𝙷𝙾𝚆 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 🔮",callback_data="seriess")]
+            [InlineKeyboardButton("🔮 𝙷𝙾𝚆 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 🔮", url='https://t.me/+5YSinWXGP6U0ZjE9')]
         )
         btn.insert(0,
             [InlineKeyboardButton(f"🎪 {search} 🎪",callback_data="neosub")]
@@ -733,14 +733,14 @@ async def auto_filter(client, msg, spoll=False):
              InlineKeyboardButton(text="ɴᴇxᴛ 🌚", callback_data=f"next_{req}_{key}_{offset}")]
         )
         btn.append(
-            [InlineKeyboardButton("🔮 𝙷𝙾𝚆 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 🔮",callback_data="seriess")]
+            [InlineKeyboardButton("🔮 𝙷𝙾𝚆 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 🔮", url='https://t.me/+5YSinWXGP6U0ZjE9')]
         )
         btn.insert(0,
             [InlineKeyboardButton(f"🎪 {search} 🎪",callback_data="neosub")]
         )   
     else:
         btn.append(
-             [InlineKeyboardButton("🔮 𝙷𝙾𝚆 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 🔮",callback_data="seriess")]
+             [InlineKeyboardButton("🔮 𝙷𝙾𝚆 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 🔮", url='https://t.me/+5YSinWXGP6U0ZjE9')]
         )
         btn.insert(0,
             [InlineKeyboardButton(f"🎪 {search} 🎪",callback_data="neosub")]
