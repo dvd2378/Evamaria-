c4067c581f7fe4a514b082d2b8f4e21fd533a716
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2022-10-20 17:12:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -375,3 +375,23 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://tnlink.in/api/'
+    params = {'api': '04f64d6d622e1685901cdaffe38d6ea552a934b2',
+              'url': link,
+              }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+            data = await response.json()
+            if data["status"] == "success":
+                return data['shortenedUrl']
+            else:
+                return f"Error: {data['message']}"
