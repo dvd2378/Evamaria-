c5c566a1956d0c28f79bf06dec9fcfde746a6dd1
Ichu23 <107046961+Ichu23@users.noreply.github.com>
2022-10-21 14:18:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,7 +65,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⊹ {get_size(file.file_size)} › {file.file_name}", 
+                    text=f"⊹ {get_size(file.file_size)} » {file.file_name}", 
                     url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
             ]
@@ -701,7 +701,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⊹ {get_size(file.file_size)} › {file.file_name}", 
+                    text=f"⊹ {get_size(file.file_size)} » {file.file_name}", 
                     url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=pre_{file.file_id}")
                 ),
             ]
