98fe470260400d60f7e782ef1cca0811d0bde578
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-10-25 19:16:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
-echo """
+echo " the end 😅"
 
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
-cd /EvaMaria
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
 
-"""
